90895b8e4a285b40ec38014481dfdbcc949cc039
Pau Ortega Puig <pau.ortegapuig@thoughtworks.com>
2022-04-22 21:37:00 +0200

    It's alive.

--- php/src/GildedRose.php
+++ php/src/GildedRose.php
@@ -6,6 +6,9 @@ namespace GildedRose;
 
 final class GildedRose
 {
+    const AGED_BRIE = 'Aged Brie';
+    const BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = 'Backstage passes to a TAFKAL80ETC concert';
+    const SULFURAS_HAND_OF_RAGNAROS = 'Sulfuras, Hand of Ragnaros';
     /**
      * @var Item[]
      */
@@ -65,5 +68,10 @@ final class GildedRose
                 }
             }
         }
+    }
+
+    public function items(): array
+    {
+        return $this->items;
     }
 }
--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,57 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
+    const HEADER_DAY = 1;
+    const HEADER_COLUMN_NAMES = 1;
+    const SEPARATING_LINE_BREAK = 1;
+    /**
+     * @var Item[]
+     */
+    private $initialItems;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $dexterityVest = new Item('+5 Dexterity Vest', 10, 20);
+        $agedBrie = new Item('Aged Brie', 2, 0);
+        $elixirOfTheMongoose = new Item('Elixir of the Mongoose', 5, 7);
+        $sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
+        $backstagePass1 = new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20);
+        $backstagePass2 = new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49);
+        $backstagePass3 = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49);
+        $conjuredManaCake = new Item('Conjured Mana Cake', 3, 6);
+
+        $this->initialItems = [
+            $dexterityVest,
+            $agedBrie,
+            $elixirOfTheMongoose,
+            $sulfuras,
+            $backstagePass1,
+            $backstagePass2,
+            $backstagePass3,
+            $conjuredManaCake
+        ];
+    }
+
     public function testFoo(): void
     {
-        $items = [new Item('foo', 0, 0)];
-        $gildedRose = new GildedRose($items);
-        $gildedRose->updateQuality();
-        $this->assertSame('fixme', $items[0]->name);
+        $file = file_get_contents('/usr/src/myapp/tests/approvals/ApprovalTest.testTestFixture.approved.txt');
+        $fileByLines = explode(PHP_EOL, $file);
+
+        $gildedRose = new GildedRose($this->initialItems);
+        $lengthOfBlock = count($this->initialItems) + self::HEADER_DAY + self::HEADER_COLUMN_NAMES + self::SEPARATING_LINE_BREAK;
+
+        for ($i = 0; $i < 30; $i++) {
+            $this->assertSame("-------- day {$i} --------", $fileByLines[$lengthOfBlock * $i]);
+            $this->assertSame("name, sellIn, quality", $fileByLines[$lengthOfBlock * $i + 1]);
+
+            $itemsToday = $gildedRose->items();
+            for ($j = 0; $j < count($itemsToday); $j++) {
+                $this->assertSame("{$itemsToday[$j]->name}, {$itemsToday[$j]->sell_in}, {$itemsToday[$j]->quality}", $fileByLines[$lengthOfBlock * $i + 2 + $j]);
+            }
+
+            $gildedRose->updateQuality();
+        }
     }
 }
